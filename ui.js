8d7e398c4194c2203d6af6ffd64f51f3483f9634
NevilleCentipede <158459644+NevilleCentipede@users.noreply.github.com>
2026-07-25 11:21:10 +0600

    Update ui.js

--- ui.js
+++ ui.js
@@ -3,15 +3,15 @@ const { div, h1, p, button , li, a, img} = van.tags
 export function githubRedirect() {window.location.assign("https://github.com/NevilleCentipede");}
 export function websiteRedirect() {window.location.assign("https://github.com/NevilleCentipede");}
 // 2. Elements Configuration
-
 export const createButtonGroup = div({class: "container", style: "margin-down: 10px;"},
   button({onclick: githubRedirect}, "github"),
   button({onclick: githubRedirect}, "website"),
   button({onclick: githubRedirect}, "github")
 )
-
+/*Title*/
 export const title = h1({ style: "margin-top: 1px; text-align: center;" },
 "Flat code, Procedural, functional style, Vanjs + animejs")
+/*why use it paragraphs*/
 export const why = div({ style: "text-align: center;" },
     p("why should one use it?"),
     p("1. It is light"),
@@ -21,7 +21,8 @@ export const why = div({ style: "text-align: center;" },
     p("5. It does not use any framework that uses a code of conduct, which I call code of leftists"),
     p("6. It is fully open source")
     )
-export const rectangle1 = img({class: "rectangle2", src:"assets/rectangle-svgrepo-com.svg", style: "margin-top: 50px"});  
-export const rectangle2 = img({class: "rectangle2", src:"assets/rectangle-svgrepo-com.svg"});
+/*Rectangles*/
+export const rectangle1 = img({class: "rectangle2", src:"rectangle-svgrepo-com.svg", style: "margin-top: 50px"});  
+export const rectangle2 = img({class: "rectangle2", src:"rectangle-svgrepo-com.svg"});
 
 
